78721cebd2cf6978125631c983216c8e1f8d27a6
Ren-Shimamura <ren.shimamura.hs@kyocera.jp>
2025-06-05 13:44:45 +0900

    Update Main.java

--- example/src/example3/Main.java
+++ example/src/example3/Main.java
@@ -1,13 +1,1 @@
-package example3;
 
-import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("円の半径を入力してください：");
-        double radius = scan.nextDouble();
- 
- 
-    }
-}
