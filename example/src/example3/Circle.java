1b65ed0930b65ba4dc2ce298dded7d18a0924983
Ren-Shimamura <ren.shimamura.hs@kyocera.jp>
2025-06-05 13:45:01 +0900

    Update Circle.java

--- example/src/example3/Circle.java
+++ example/src/example3/Circle.java
@@ -1,14 +1,1 @@
-package example3;
 
-public class Circle {
-    public static final double PI = 3.14;
-    
-    
-    public Circle(double radius) {
-
-    }
-
-    public double area() {
-
-    }
-}
