f016af88237dc6283c95d57c2fc9266267a01800
github Ren-Shimamura <ren.shimamura.hs@kyocera.jp>
2025-06-05 13:06:48 +0900

    Java例題：try-catch構文

--- example/src/example2/Main.java
+++ example/src/example2/Main.java
@@ -1,10 +1,22 @@
 package example2;
+import java.util.Scanner;
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
-
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("1つ目の小数を入力してください：");
+        String numStr1 = scan.nextLine();
+        System.out.print("2つ目の小数を入力してください：");
+        String numStr2 = scan.nextLine();
+ 
+        try {
+		    double num1 = Double.parseDouble(numStr1);
+            double num2 = Double.parseDouble(numStr2);
+            System.out.println( "合計は"+(num1+num2)+"です");
+		    
+		} catch (NumberFormatException e) {
+		    System.out.println("入力された値は数値ではありません");
+		    
+		}
+    }
 }
