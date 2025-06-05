499533914baa080adda6c02d6836baca326d4710
github Ren-Shimamura <ren.shimamura.hs@kyocera.jp>
2025-06-05 13:28:27 +0900

    初回コミット

---
+++ example/src/example3/Circle.java
@@ -0,0 +1,14 @@
+package example3;
+
+public class Circle {
+    public static final double PI = 3.14;
+    
+    
+    public Circle(double radius) {
+
+    }
+
+    public double area() {
+
+    }
+}
---
+++ example/src/example3/Main.java
@@ -0,0 +1,13 @@
+package example3;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("円の半径を入力してください：");
+        double radius = scan.nextDouble();
+ 
+ 
+    }
+}
